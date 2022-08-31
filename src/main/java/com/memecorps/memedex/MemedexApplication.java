6b01f6567d17e2e91569c75b01ece7f35382dabb
Stephen Tondel <stondel@manifestcrop.com>
2022-08-31 14:30:22 -0400

    Initial commit

---
+++ src/main/java/com/memecorps/memedex/MemedexApplication.java
@@ -0,0 +1,13 @@
+package com.memecorps.memedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemedexApplication.class, args);
+	}
+
+}
